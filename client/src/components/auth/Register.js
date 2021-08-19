62742c0f91022780d375d9d605fad67314d67a8d
hung nguyen <hungnvh96@gmail.com>
2021-08-19 10:35:26 +0300

    add register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,117 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-	return <div>register</div>;
-	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#computed_property_names
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		password2: "",
+	});
+	const { name, email, password, password2 } = formData;
+
+	const onChange = (e) => {
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#computed_property_names
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		if (password !== password2) {
+			console.log("password not match");
+		} else {
+			console.log("success");
+			// console.log(formData);
+			// const newUser = {
+			// 	name: name,
+			// 	email: email,
+			// 	password: password,
+			// 	password2: password2,
+			// };
+			// try {
+			// 	let axiosConfig = {
+			// 		headers: {
+			// 			"Content-Type": "application/json;charset=UTF-8",
+			// 			"Access-Control-Allow-Origin": "*",
+			// 		},
+			// 	};
+			// 	const body = JSON.stringify(newUser);
+			// 	const res = await axios.post(
+			// 		"/api/users",
+			// 		newUser,
+			// 		axiosConfig
+			// 	);
+			// 	console.log(res.data);
+			// } catch (err) {
+			// 	console.error(err.response.data);
+			// }
+		}
+	};
+
+	return (
+		<Fragment>
+			<h1 className="large text-primary">Sign Up</h1>
+			<p className="lead">
+				<i className="fas fa-user"></i> Create Your Account
+			</p>
+			<form onSubmit={onSubmit} className="form">
+				<div className="form-group">
+					<input
+						type="text"
+						placeholder="Name"
+						name="name"
+						value={name}
+						required
+						onChange={onChange}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
+					<small className="form-text">
+						This site uses Gravatar so if you want a profile image,
+						use a Gravatar email
+					</small>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						minLength="6"
+						value={password}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Confirm Password"
+						name="password2"
+						minLength="6"
+						value={password2}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<input
+					type="submit"
+					className="btn btn-primary"
+					value="Register"
+				/>
+			</form>
+			<p className="my-1">
+				Already have an account? <a href="login.html">Sign In</a>
+			</p>
+		</Fragment>
+	);
 };
 
 export default Register;
